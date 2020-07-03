dda9deb0b220f32a1cb099479343678bd9e9e3aa
Sai Krithik <pvsaikrithik@gmail.com>
2020-07-03 14:26:49 +0530

    update Final.py

--- Final.py
+++ Final.py
@@ -1,4 +1,8 @@
+# USAGE
+# python Final.py --shape-predictor shape_predictor_68_face_landmarks.dat
+# python Final.py --shape-predictor shape_predictor_68_face_landmarks.dat --alarm alarm.wav
 
+# import the necessary packages
 from scipy.spatial import distance as dist
 from imutils.video import VideoStream
 from imutils import face_utils
@@ -14,133 +18,147 @@ import os
 
 os.system('python Test.py')
 
-faceCascade = cv2.CascadeClassifier('Haar/haarcascade_frontalface_default.xml')
 
 
 def sound_alarm(path):
-        playsound.playsound(path)
+	# play an alarm sound
+	playsound.playsound(path)
 
 def eye_aspect_ratio(eye):
+	# compute the euclidean distances between the two sets of
+	# vertical eye landmarks (x, y)-coordinates
+	A = dist.euclidean(eye[1], eye[5])
+	B = dist.euclidean(eye[2], eye[4])
 
-        A = dist.euclidean(eye[1], eye[5])
-        B = dist.euclidean(eye[2], eye[4])
-        C = dist.euclidean(eye[0], eye[3])
-        ear = (A + B) / (2.0 * C)
+	# compute the euclidean distance between the horizontal
+	# eye landmark (x, y)-coordinates
+	C = dist.euclidean(eye[0], eye[3])
 
-        return ear
+	# compute the eye aspect ratio
+	ear = (A + B) / (2.0 * C)
+
+	# return the eye aspect ratio
+	return ear
  
-
+# construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-w", "--webcam", type=int, default=0,
-        help="index of webcam on system")
+	help="index of webcam on system")
 args = vars(ap.parse_args())
  
-
+# define two constants, one for the eye aspect ratio to indicate
+# blink and then a second constant for the number of consecutive
+# frames the eye must be below the threshold for to set off the
+# alarm
 EYE_AR_THRESH = 0.3
 EYE_AR_CONSEC_FRAMES = 48
 
-
+# initialize the frame counter as well as a boolean used to
+# indicate if the alarm is going off
 COUNTER = 0
 ALARM_ON = False
 
-
+# initialize dlib's face detector (HOG-based) and then create
+# the facial landmark predictor
 print("[INFO] loading facial landmark predictor...")
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 
-
+# grab the indexes of the facial landmarks for the left and
+# right eye, respectively
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
-
+# start the video stream thread
 print("[INFO] starting video stream thread...")
-vs = cv2.VideoCapture(0)
+vs = VideoStream(src=args["webcam"]).start()
 #time.sleep(1.0)
-i=0
-loop=[]
-person=True
 
-
+# loop over frames from the video stream
 while True:
+	# grab the frame from the threaded video file stream, resize
+	# it, and convert it to grayscale
+	# channels)
+	frame = vs.read()
+	frame = imutils.resize(frame, width=450)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        ret,frame = vs.read()
-        frame = imutils.resize(frame, width=450)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = faceCascade.detectMultiScale(gray)
-        loop.append(faces)
-        print(person)
-        # detect faces in the grayscale frame
-        rects = detector(gray, 0)
-        i=i+1
-        if i>40:
-                for j in range(i-40,i):
-                        if len(loop[j]) == 0:
-                                person=False
-                                
-                                cv2.destroyAllWindows()
-                                vs.release()
-                                os.system('python Final.py')
-                                
-                        else:
-                                person=True
+	# detect faces in the grayscale frame
+	rects = detector(gray, 0)
 
+	# loop over the face detections
+	for rect in rects:
+		# determine the facial landmarks for the face region, then
+		# convert the facial landmark (x, y)-coordinates to a NumPy
+		# array
+		shape = predictor(gray, rect)
+		shape = face_utils.shape_to_np(shape)
 
-        rects = detector(gray, 0)
+		# extract the left and right eye coordinates, then use the
+		# coordinates to compute the eye aspect ratio for both eyes
+		leftEye = shape[lStart:lEnd]
+		rightEye = shape[rStart:rEnd]
+		leftEAR = eye_aspect_ratio(leftEye)
+		rightEAR = eye_aspect_ratio(rightEye)
 
+		# average the eye aspect ratio together for both eyes
+		ear = (leftEAR + rightEAR) / 2.0
 
-        for rect in rects:
+		# compute the convex hull for the left and right eye, then
+		# visualize each of the eyes
+		leftEyeHull = cv2.convexHull(leftEye)
+		rightEyeHull = cv2.convexHull(rightEye)
+		cv2.drawContours(frame, [leftEyeHull], -1, (255, 255, 255), 1)
+		cv2.drawContours(frame, [rightEyeHull], -1, (255, 255, 255), 1)
 
-                shape = predictor(gray, rect)
-                shape = face_utils.shape_to_np(shape)
+		# check to see if the eye aspect ratio is below the blink
+		# threshold, and if so, increment the blink frame counter
+		if ear < EYE_AR_THRESH:
+			COUNTER += 1
 
+			# if the eyes were closed for a sufficient number of
+			# then sound the alarm
+			if COUNTER >= EYE_AR_CONSEC_FRAMES:
+				# if the alarm is not on, turn it on
+				if not ALARM_ON:
+					ALARM_ON = True
+					os.system('python adminsleepy.py')
 
-                leftEye = shape[lStart:lEnd]
-                rightEye = shape[rStart:rEnd]
-                leftEAR = eye_aspect_ratio(leftEye)
-                rightEAR = eye_aspect_ratio(rightEye)
+					# check to see if an alarm file was supplied,
+					# and if so, start a thread to have the alarm
+					# sound played in the background
+					if 'alarm2.mp3' != "":
+						t = Thread(target=sound_alarm,
+							args=('alarm2.mp3',))
+						t.deamon = True
+						t.start()
 
+				# draw an alarm on the frame
+				cv2.putText(frame, "DROWSINESS ALERT!", (10, 30),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
-                ear = (leftEAR + rightEAR) / 2.0
+		# otherwise, the eye aspect ratio is not below the blink
+		# threshold, so reset the counter and alarm
+		else:
+			COUNTER = 0
+			ALARM_ON = False
 
-
-                leftEyeHull = cv2.convexHull(leftEye)
-                rightEyeHull = cv2.convexHull(rightEye)
-                cv2.drawContours(frame, [leftEyeHull], -1, (255, 255, 255), 1)
-                cv2.drawContours(frame, [rightEyeHull], -1, (255, 255, 255), 1)
-
-                if ear < EYE_AR_THRESH:
-                        COUNTER += 1
-
-
-                        if COUNTER >= EYE_AR_CONSEC_FRAMES:
-                                if not ALARM_ON:
-                                        ALARM_ON = True
-                                        os.system('python adminsleepy.py')
-
-
-                                        if 'alarm2.mp3' != "":
-                                                t = Thread(target=sound_alarm,
-                                                        args=('alarm2.mp3',))
-                                                t.deamon = True
-                                                t.start()
-
-                                cv2.putText(frame, "DROWSINESS ALERT!", (10, 30),
-                                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-
-
-                else:
-                        COUNTER = 0
-                        ALARM_ON = False
-                cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
+		# draw the computed eye aspect ratio on the frame to help
+		# with debugging and setting the correct eye aspect ratio
+		# thresholds and frame counters
+		cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
  
-        cv2.imshow("Frame", frame)
-        key = cv2.waitKey(1) & 0xFF
+	# show the frame
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
  
-        if key == ord("q"):
-                break
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
 
-
+# do a bit of cleanup
 cv2.destroyAllWindows()
-vs.release()
+vs.stop()
 
+# python Final.py --shape-predictor shape_predictor_68_face_landmarks.dat --alarm alarm2.mp3
